45c5a9efca91f580f004042c2e70facc08c06261
Monish-vit <monishwar.s2023@vitstudent.ac.in>
2026-03-02 21:02:08 +0530

    Update greeting and replace with simple calculator addn

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,18 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Agile Maven!");
+        System.out.println("--- Agile Calculator System ---");
+        App calc = new App();
+        System.out.println("Addition (10 + 5): " + calc.add(10, 5));
     }
-    
-    public String getGreeting() {
-        return "Hello World";
+
+    // Simple Addition Method
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Simple Subtraction Method
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
