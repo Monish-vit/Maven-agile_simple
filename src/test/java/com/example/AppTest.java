e604d87fcdae5d8b68f7631b8fab2f32e3c36b95
Monish-vit <monishwar.s2023@vitstudent.ac.in>
2026-03-02 21:05:01 +0530

    Refactor AppTest to include addition and subtraction tests

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,21 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
-            import org.junit.Test;
+import org.junit.Test;
 
 public class AppTest {
+    
     @Test
-    public void testGreeting() {
-        App app = new App();
-        assertEquals("Hello World", app.getGreeting());
+    public void testAddition() {
+        App calc = new App();
+        // We expect 10 + 5 to equal 15
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtraction() {
+        App calc = new App();
+        // We expect 20 - 8 to equal 12
+        assertEquals(12, calc.subtract(20, 8));
     }
 }
